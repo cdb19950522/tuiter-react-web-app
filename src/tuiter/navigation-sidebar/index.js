ba069a2c287e395a4c5bf723a634b1f649754c5b
cdb19950522 <dbchenchina@gmail.com>
2022-10-22 19:34:30 -0400

    fix href

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -8,7 +8,7 @@ const NavigationSidebar = () => {
     const active = paths[2];
     return (
         <div className="list-group">
-            <a className="list-group-item"><i className="bi bi-twitter"></i> Tuiter</a>
+            <a href="/#" className="list-group-item"><i className="bi bi-twitter"></i> Tuiter</a>
             <Link to="/tuiter/home" className={`list-group-item
                     ${active === 'home'?'active':''}`}>
                 <i className="bi bi-house-fill"></i>&nbsp;Home
@@ -22,37 +22,37 @@ const NavigationSidebar = () => {
             <Link to="/" className="list-group-item">
                 <i className="bi bi-cup-straw"></i> &nbsp;Labs
             </Link>
-            <a className={`list-group-item
+            <a href="/#" className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
                 <i className="bi bi-bell-fill"></i>
                 &nbsp;
                 Notifications
             </a>
-            <a className={`list-group-item
+            <a href="/#" className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
                 <i className="bi bi-envelope-fill"></i>
                 &nbsp;
                 Messages
             </a>
-            <a className={`list-group-item
+            <a href="/#" className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
                 <i className="bi bi-bookmark-fill"></i>
                 &nbsp;
                 Bookmarks
             </a>
-            <a className={`list-group-item
+            <a href="/#" className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
                 <i className="bi bi-list-ul"></i>
                 &nbsp;
                 Lists
             </a>
-            <a className={`list-group-item
+            <a href="/#" className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
                 <i className="bi bi-person-fill"></i>
                 &nbsp;
                 Profile
             </a>
-            <a className={`list-group-item
+            <a href="/#" className={`list-group-item
                     ${active === 'more'?'active':''}`}>
                 <i className="bi bi-three-dots"></i>
                 &nbsp;
